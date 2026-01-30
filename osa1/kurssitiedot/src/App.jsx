b978804297bfbb383043224afff1950c6c3e409d
Lauri Mäkynen <laurimakynen@Lauri-MacBook-Pro-26.local>
2026-01-30 10:41:48 +0200

    Lisätty props refaktorointiin

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,30 +1,30 @@
 //Kurssin nimi täältä
-const Header = ({ course}) => {
+const Header = ({course}) => {
   return <h1>{course}</h1>
 }
 
-// Luodaan Part osa 1.2 osiota varten
-const Part = ({name, exercises}) => {
+// Luodaan Part osa 1.2 osiota varten käyttäen props
+const Part = (props) => {
   return (
     <p>
-      {name} {exercises}
+      Part {props.name} {props.exercises}
     </p>
   )
 }
 //Header, kurssitiedoille. Nämä haetaan App komponentissa siten. 
-//Toteutettu 1.2 kohdan mukainen refakrorointi. 
-const Content = ({part1, exercises1, part2, exercises2, part3, exercises3 }) =>{
+//Toteutettu 1.2 kohdan mukainen refakrorointi käyttäen props. 
+const Content = (props) =>{
   return(
     <div>
-      <Part name={part1} exercises={exercises1}/>
-      <Part name={part2} exercises={exercises2}/>
-      <Part name={part3} exercises={exercises3}/>
+      <Part name={props.part1} exercises={props.exercises1}/>
+      <Part name={props.part2} exercises={props.exercises2}/>
+      <Part name={props.part3} exercises={props.exercises3}/>
     </div>
   )
 }
 //Luodaan total, joka viedään App komponenttiin
-const Total = ({total}) => {
-  return <p> Number of exercises {total} </p>
+const Total = (props) => {
+  return <p> Number of exercises {props.total} </p>
 }
 
 //Korjattu App komponentti, joka hakee Header, Content ja Total komponentteihin tallenntetut tiedot. 
