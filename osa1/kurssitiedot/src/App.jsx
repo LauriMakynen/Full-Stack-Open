f35b625a00d95409c23e87e8039bb3d7a959bcdf
Lauri Mäkynen <laurimakynen@Lauri-MacBook-Pro-26.local>
2026-01-30 12:40:31 +0200

    Tehty 1.4 tehtävä

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,64 +1,64 @@
-//Kurssin nimi täältä
-const Header = ({course}) => {
-  return <h1>{course}</h1>
+//Luoti header prps avulla
+const Header = (props) => {
+  console.log(props)
+  return <h1>{props.course}</h1>
 }
-
-// Luodaan Part osa 1.2 osiota varten käyttäen props
+console.log("Onnistuuko")
+//Määritellään olio "part", joka koostuu nimestä & tehtävistä
 const Part = (props) => {
   return (
     <p>
-      Part {props.name} {props.exercises}
+      Part {props.part.name} {props.part.exercises}
     </p>
   )
 }
-//Header, kurssitiedoille. Nämä haetaan App komponentissa siten. 
-//Toteutettu 1.2 kohdan mukainen refakrorointi käyttäen props. 
+
+//....................................
 const Content = (props) =>{
   return(
     <div>
-      <Part name={props.part1} exercises={props.exercises1}/>
-      <Part name={props.part2} exercises={props.exercises2}/>
-      <Part name={props.part3} exercises={props.exercises3}/>
+      <Part part={props.parts[0]}/>
+      <Part part={props.parts[1]}/>
+      <Part part={props.parts[2]}/>
     </div>
   )
 }
-//Luodaan total, joka viedään App komponenttiin
+//.............................................
 const Total = (props) => {
-  return <p> Number of exercises {props.total} </p>
-}
+  return <p>
+  Number of exercises {''}
+   {props.parts[0].exercises + 
+   props.parts[1].exercises + 
+   props.parts[2].exercises} 
+  </p>
+} 
 
-//Korjattu App komponentti, joka hakee Header, Content ja Total komponentteihin tallenntetut tiedot. 
+//...................................................
 
 const App = () => {
   const course = 'Half Stack application development'
+  const parts = [
+  { 
+  name: 'Fundamentals of React',
+  exercises: 10
+  },
+  { 
+  name: 'Using props to pass data',
+  exercises: 7
+  },
+  {
+  name: 'State of a component',
+  exercises: 14
+  }
+]
 
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  const total = exercises1 + exercises2 +exercises3
   return (
     <div>
       <Header course={course} />
-      <Content 
-      part1={part1}
-      exercises1={exercises1}
-
-      part2={part2}
-      exercises2={exercises2}
-
-      part3={part3}
-      exercises3={exercises3}
-      />
-      <Total total={total}/>
+      <Content parts = {parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
-
 
 export default App
