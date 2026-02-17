8f5531943fee5e8255ccb1e5ff0c369b3ea08545
Lauri Mäkynen <laurimakynen@Lauri-MacBook-Pro-26.local>
2026-02-17 11:27:34 +0200

    Tehty 1.7 vaihe ja aloitettu 1.8 vaihetta, consoli herjaa jotain virhettä

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -2,6 +2,26 @@ import { useState } from 'react'
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
+const Statistics = ({good, neutral, bad}) => {
+const all = good + neutral + bad
+if (all ===0)
+  return(
+  <div> 
+    no feedback given
+  </div>)
+
+ const average = (good - bad) / all
+ const positive = (good / all)
+
+return(
+  <div>
+  <div> all {all}</div>
+  <div> average {average}</div>
+  <div> positive {positive}</div>
+  </div>
+)
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -14,10 +34,13 @@ const App = () => {
       <Button onClick={() => setGood(good + 1)} text="good" />
       <Button onClick={() => setNeutral(neutral +1)} text="neutral" />   
       <Button onClick={() => setBad(bad +1)} text="bad" />
+        
     <h2>statistics</h2>
     <div> good {good}</div>
     <div> neutral {neutral}</div>
     <div> bad {bad}</div>
+    <h2>statistics</h2>
+    <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
