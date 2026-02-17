4d125a1199ea91a2f29181bb7c5eb7492d175d76
Lauri Mäkynen <laurimakynen@Lauri-MacBook-Pro-26.local>
2026-02-17 15:22:05 +0200

    Nyt lisätty tiedosto 1.12-1.14 tehtäville, vanhaan tiedostoon unicafe jäi vahingossa pushiin anekdootit

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -34,18 +34,37 @@ const Statistics = (props) => {
 }
 
 const App = () => {
+  //Tämä lisättiin vahingossa... 
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  //Random anecdootin valitsija. 
+  const nextAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+  }
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [selected, setSelected] = useState(0)
 
   return (
     <div>
+      {anecdotes[selected]}
+      <Button onClick={nextAnecdote} text="Next Anecdote" />
       <h1>Give Feedback</h1>
 
       <Button onClick={() => setGood(good + 1)} text="good" />
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
-
       <h2>statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
