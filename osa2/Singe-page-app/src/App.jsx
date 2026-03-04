85807acbee86600597d6dd1833a6e309b0b3dc3b
LauriMakynen <lauri.makyn3n@gmail.com>
2026-03-04 12:27:16 +0200

    Tehty 2.10

--- osa2/Singe-page-app/src/App.jsx
+++ osa2/Singe-page-app/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Note from './components/Note'
+import axios from 'axios'
 
-
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+
+
+useEffect(() => {
+  console.log('effect')
+  axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+}, [])
+console.log('render', notes.length, 'notes')  
+
 
   const addNote = (event) => {
     event.preventDefault()
@@ -17,8 +30,6 @@ const App = (props) => {
     setNotes(notes.concat(noteObject))
     setNewNote('')
   }
-
-
 
     const handleNoteChange = (event) => {
       console.log(event.target.value)
