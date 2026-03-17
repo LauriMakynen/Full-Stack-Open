c8ab97021e3be8cfd92cbd083127bf0f299d0393
LauriMakynen <lauri.makyn3n@gmail.com>
2026-03-17 19:12:32 +0200

    Tehty tehtävä 3.20

--- osa3/puhelinluettelo/models/person.js
+++ osa3/puhelinluettelo/models/person.js
@@ -31,6 +31,11 @@ const personSchema = new mongoose.Schema({
   number: {
     type: String,
     required: true,
+    minlength: [8, 'phone number must be at least 8 characters long'],
+    validate: {
+      validator: (value) => /^(\d{2}-\d{5,}|\d{3}-\d{4,})$/.test(value),
+      message: 'phone number must be in format XX-XXXX... or XXX-XXXX...'
+    }
   },
 })
 
@@ -45,33 +50,3 @@ personSchema.set('toJSON', {
 
 
 module.exports = mongoose.model('Person', personSchema)
-
-
-/*
-const mongoose = require('mongoose')
-
-
-//Tarkistetaan, että komentoriviparametreja on annettu riittävästi. Jos parametreja on vähemmän kuin kolme, tulostetaan ohje ja lopetetaan ohjelma.
-const password = process.argv[2]
-const url = `mongodb+srv://Harjoitus1:${password}@cluster1.wc5bnd7.mongodb.net/puhelinluettelo?retryWrites=true&w=majority`
-
-mongoose.set('strictQuery',false)
-mongoose.connect(url, { family: 4 })
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
-
-const Person = mongoose.model('Person', personSchema)
-
-//Määritellään toJSON-muunnos, joka muuttaa MongoDB:n _id-kentän id-kentäksi ja poistaa _id- ja __v-kentät JSON-muodossa.
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-*/
